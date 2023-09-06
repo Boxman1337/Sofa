e35b904833762f524f83deb49b746e73d6d3c3f9
Axel <axel.o.nilsson@gmail.com>
2023-09-06 19:31:16 +0200

    Messed around with curses.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <curses.h>
-#include <iostream>
+
+#include <string>
 
 int main(int argC, char **argV) {
-	
+
+	// Variable declarations
+	int yMax, xMax;
+	char input; 
+
+	// Initialize and set options		
 	initscr();
+	noecho();
+	curs_set(0);
+
+	// Initialize variables
+	getmaxyx(stdscr, yMax, xMax); 
+	
+	// Declare Menu window
+	WINDOW *win = newwin(yMax*3/10, xMax*8/10, yMax*7/10, xMax/10);
+	box(win, 0, 0);
+	
+	
+
+	// Print Menu titles
+	mvwprintw(win, 0, 2, "Menu");
+
+	// Main loop
+	while(true) {
+		input = wgetch(win);
+		
+		switch(input) {
+			
+			case: 
+
+		}	
+	
+	}
+	
+	endwin();
+
+	return 0;
 	
 
 }
